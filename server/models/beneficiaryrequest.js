252b534f267472959f311bdba446875c3754a5ca
Kiprop-Kimutai <jonah.kimutai@compulynx.com>
2018-12-18 22:48:42 +0300

    #modify to conform v1.2

--- server/models/beneficiaryrequest.js
+++ server/models/beneficiaryrequest.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 //create schema
 const beneficiryRequestSchema = new Schema({
+    transOperation:String,
     iccid:String,
     transId:{type:String,unique:true},
     requestId:String,
