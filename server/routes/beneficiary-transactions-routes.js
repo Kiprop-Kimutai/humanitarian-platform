d1da1dc688d41a2d5c7322018f933c15c5641912
Kiprop-Kimutai <jonah.kimutai@compulynx.com>
2018-12-18 23:18:23 +0300

    #v1.2

--- server/routes/beneficiary-transactions-routes.js
+++ server/routes/beneficiary-transactions-routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const BeneficiaryTxn = mongoose.model("BeneficiaryTxn");
-const ApiResponse = require('../models/response');
+//const ApiResponse = require('../models/response');
+const BeneficiaryTxnResponse = require('../models/apiresponse/beneficiarytxnresponse');
 const app = express();
 
 app.get("/", (req,res,next) =>{
@@ -10,16 +11,20 @@ app.get("/", (req,res,next) =>{
 
 app.post('/beneficiarytxn',(req,res,next) =>{
     var beneficiaryTxn = new BeneficiaryTxn(req.body.benTxn);
-    beneficiaryTxn.save((err)=>{
-        if(err){
-            console.error(err);
-            res.json(new ApiResponse(false,"153","transaction operation failed"));
-        }
-        else{
-            console.log("Transaction successfull");
-            res.json(new ApiResponse(true,'000',"success"));
-        }
-    })
+    for(var i=0;i<beneficiaryTxn.length;i++){
+        beneficiaryTxn[i].save((err)=>{
+            if(err){
+                console.error(err);
+                //res.json(new ApiResponse(false,"153","transaction operation failed"));
+                res.json(new BeneficiaryTxnResponse(false,"200",0,"card blocked"))
+            }
+            else{
+                console.log("Transaction successfull");
+                //res.json(new ApiResponse(true,'000',"success"));
+                res.json(new BeneficiaryTxnResponse(true,"100",1,"successful"))
+            }
+        })
+    }
 })
 
 module.exports = app;
