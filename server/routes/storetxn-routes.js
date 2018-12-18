83ef7a8a6e300d10fee91fab1d04ffcf43e2a4c7
Kiprop-Kimutai <jonah.kimutai@compulynx.com>
2018-12-19 00:22:41 +0300

    #store txns logic

--- server/routes/storetxn-routes.js
+++ server/routes/storetxn-routes.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 //const ApiResponse = require('../models/response');
 const StoreTxnResponse = require('../models/apiresponse/storetxnresponse');
 const StoreTxnSchema = mongoose.model("StoreTxnSchema");
+//const StoreBalanceRequest = mongoose.model("StoreBalanceRequest");
 const app = express();
 
 app.get('/',(req,res,next)=>{
@@ -20,6 +21,20 @@ app.post('/storetxn',(req,res,next) =>{
             console.log("txn success");
             //res.json(new ApiResponse(true,"000","success"));
             res.json(new StoreTxnResponse(true,"000",2000,1000,"success"));
+        }
+    })
+})
+
+app.post('/requestbalance',(req,res,next)=>{
+    var storeBalanceRequest = new StoreBalanceRequest(req.body.storeBalanceRequest);
+    storeBalanceRequest.save((err)=>{
+        if(err){
+            console.error(err);
+            res.json(new StoreTxnResponse(true,"400","","","failed/storeRequest failed"));
+
+        }
+        else{
+            res.json(new StoreTxnResponse(true,"153","","","success/storeRequest success"));
         }
     })
 })
