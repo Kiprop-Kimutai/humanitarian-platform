2fdd946a37f26a03f24429acaf046d692bb01d86
Kiprop-Kimutai <jonah.kimutai@compulynx.com>
2018-12-18 23:35:36 +0300

    #store txn v1.2

--- server/routes/storetxn-routes.js
+++ server/routes/storetxn-routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const ApiResponse = require('../models/response');
+//const ApiResponse = require('../models/response');
+const StoreTxnResponse = require('../models/apiresponse/storetxnresponse');
 const StoreTxnSchema = mongoose.model("StoreTxnSchema");
 const app = express();
 
@@ -13,11 +14,12 @@ app.post('/storetxn',(req,res,next) =>{
     storeTxnSchema.save((err)=>{
         if(err){
             console.error(err);
-            res.json(new ApiResponse(false,"300","failed to process"));
+            res.json(new StoreTxnResponse(false,322,"","","Failed store transactions"));
         }
         else{
             console.log("txn success");
-            res.json(new ApiResponse(true,"000","success"));
+            //res.json(new ApiResponse(true,"000","success"));
+            res.json(new StoreTxnResponse(true,"000",2000,1000,"success"));
         }
     })
 })
