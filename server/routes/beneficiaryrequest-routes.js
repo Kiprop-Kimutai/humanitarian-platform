f739cff9b4b2abc0a4494f41e21dccc1e1e8519a
Kiprop-Kimutai <jonah.kimutai@compulynx.com>
2018-12-18 22:51:50 +0300

    #v1.2

--- server/routes/beneficiaryrequest-routes.js
+++ server/routes/beneficiaryrequest-routes.js
@@ -1,23 +1,40 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const BeneficiaryRequest = mongoose.model("BeneficiaryRequest");
-const ApiResponse = require('../models/response');
+//const ApiResponse = require('../models/response');
+const BeneficiaryRequestResponse = require("../models/apiresponse/beneficiaryrequestresponse");
 const app = express();
 
 app.get('/',(req,res,next) =>{
     res.send("beneficiary request.....");
 })
 
-app.post('beneficiaryrequest',(req,res,next) =>{
+app.post('/programmeupdate',(req,res,next) =>{
     var beneficiaryRequest = new BeneficiaryRequest(req.body.benfRequest);
     beneficiaryRequest.save((err)=>{
         if(err){
             console.error(err);
-            res.json(new ApiResponse(false,"failed"));
+            //res.json(new ApiResponse(false,"failed"));
+            res.json(false,"200",0,"Card is blocked",[]);
         }
         else{
             console.log("Beneficiary request sucsess");
-            res.json(new ApiResponse(true,[{"100":2200}]));
+           // res.json(new ApiResponse(true,[{"100":2200}]));
+            res.json(new BeneficiaryRequestResponse(true,"100",1,"Success",{"status": true,"responseCode": "100","cardStatus": 1,"responseMessage": "Success","responseObj": {"balances": [{"walletName": "WFP","WalletBalance": 3500,"walletID": "100","currency": "Ksh"},{"walletName": "IOM","WalletBalance": 4000,"walletID": "200","currency": "Ksh"}]}}));
+        }
+    })
+})
+
+app.post('/ministatement',(req,res,next) =>{
+    var beneficiaryRequest = new BeneficiaryRequest(req.body.benfRequest);
+    benfRequest.save((err)=>{
+        if(err){
+            console.error(err);
+            res.json(false,"200",0,"Card is blocked",[]);
+        }
+        else{
+            console.log("ministatement fetched successfully");
+            res.json(true,"100",1,"success",{"status": true,"responseCode": "100","cardStatus": 1,"responseMessage": "Success","responseObj": {"transactions": [{"transOperation": "111","amount": 1027,"debitAccountNo": "8cg4382017583718","creditAccountNo": "8cg4382017583719","transId": "BT-75873937","walletName": "100","walletID": "100","walletBalance": 100,"terminalId ": "82346731 ","authMode": "100"}, {"transOperation": "111","amount": 1027,"debitAccountNo": "8cg4382017583718","creditAccountNo": "8cg4382017583719","transId": "BT-758739323237","walletName": "100","walletID": "100","walletBalance": 100,"terminalId ": "82346731 ","authMode": "100"}]}});
         }
     })
 })
